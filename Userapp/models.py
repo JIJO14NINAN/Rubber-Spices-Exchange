d86c83465537f73ce749e44e8086704652226671
Adithya-Sidharth <adithyasid2002@gmail.com>
2025-07-16 21:17:51 +0530

    Removed Order related functions

--- Userapp/models.py
+++ Userapp/models.py
@@ -2,18 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Order_Master(models.Model):
-    uid=models.ForeignKey('Adminapp.Reg',on_delete=models.CASCADE)
-    order_date=models.DateField()
-    gtotal=models.IntegerField()
-
-class Order_Child(models.Model):
-    order_id=models.ForeignKey(Order_Master,on_delete=models.CASCADE)
-    pid=models.ForeignKey('Adminapp.Admin_Products',on_delete=models.CASCADE)
-    no_of_items=models.IntegerField()
-    total=models.IntegerField()
-    status=models.CharField(max_length=10)
-
 class User_Products(models.Model):
     uid=models.ForeignKey('Adminapp.Reg',on_delete=models.CASCADE)
     cid=models.ForeignKey('Adminapp.Cattegory',on_delete=models.CASCADE)
--- Userapp/views.py
+++ Userapp/views.py
@@ -10,9 +10,6 @@ def userhome(request):
     context={}
     return HttpResponse(template.render(context,request))
 
-
-
-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import User_Products
@@ -153,110 +150,8 @@ def product_details_view(request,id):
 
 
 from datetime import date
-from .models import Order_Master, Order_Child
-from django.db import transaction
-
-def cart_view(request):
-    if request.method == 'POST':
-        c1 = Cart.objects.all()
-        c = Order_Master()
-        c.uid = Reg.objects.get(id=request.session['uid'])
-        c.order_date = date.today()
-        c.gtotal = request.POST['total']  
-        c.save()
-
-        for i in c1:
-            oc = Order_Child()
-            oc.order_id = c
-            oc.pid = i.pid
-            oc.no_of_items = i.no_of_items
-            oc.total = i.total
-            oc.status = "pending"
-            oc.save()
-            subject = "Order Confirmation"
-            message = "Your order has been placed successfully. Thank you for shopping with us!"
-            from django.conf import settings
-            email_from = settings.EMAIL_HOST_USER
-            from django.core.mail import send_mail
-            subject = "Order Confirmation"  
-            mailid = request.POST.get('email')
-            recipient_list = [mailid, ]
-            send_mail(subject, message, email_from, recipient_list)
-        # Clear the cart after placing the order
-        for item in c1:
-            item.delete()
-        return HttpResponse("<script>alert('Order placed successfully!');window.location='/orders_view/';</script>")
-    else:
-        c = Cart.objects.filter(uid=request.session['uid'])
-        total = 0
-        for i in c:
-            total += i.total
-        template = loader.get_template("cart.html")
-        context = {'cart': c, 'total': total}
-        return HttpResponse(template.render(context, request))        
-
-
-
-    
+from django.db import transaction   
 from Adminapp.models import Cart
 
-def add_to_cart(request):
-    if request.method == 'POST':
-        c = Cart()
-        c.uid = Reg.objects.get(id=request.session['uid'])
-        c.pid = Admin_Products.objects.get(id=request.POST['pid'])
-        c.no_of_items = int(request.POST.get('no_of_items'))
-        c.total = int(c.pid.rate) * int(c.no_of_items)
-        c.save()
-        return HttpResponse("<script>alert('Product added to cart successfully!');window.location='/view_products/';</script>")
-
-      
-def cart_remove(request,id):
-    cr = Cart.objects.get(id=id)
-    cr.delete()
-    return HttpResponse("<script>alert('Product removed from cart successfully!');window.location='/cart_view/';</script>")
-
-def cancel_order(request,id):
-   cr = Order_Child.objects.get(id=id)
-   cr1 = Order_Master.objects.get(id=cr.order_id.id)
-   cr.delete()
-   return HttpResponse("<script>alert('Order cancelled successfully!');window.location='/view_products/';</script>")
-
-def orders_view(request):
-    template=loader.get_template("orders_view.html")
-    context={}
-    return HttpResponse(("<script>alert('Order placed successfully!');window.location='/view_products/';</script>") + template.render(context,request))
-
-
-def user_orders(request):
-    user_id = request.session.get('uid')
-    if not user_id:
-        return HttpResponse("<script>alert('User not logged in!');window.location='/login/';</script>")
-    orders = Order_Master.objects.filter(uid=user_id)
-    order_details = []
-    for order in orders:
-        order_items = Order_Child.objects.filter(order_id=order.id)
-        order_details.append({
-            'order': order,
-            'items': order_items,
-        })
-
-    context = {'order_details': order_details}
-    return render(request, 'user_orders.html', context)
-
-
-
 def admin_all_user_orders(request):
-    orders = Order_Master.objects.all().select_related('uid')
-    all_order_details = []
-    for order in orders:
-        order_items = Order_Child.objects.filter(order_id=order.id).select_related('pid')
-        for item in order_items:
-            all_order_details.append({
-                'user_name': order.uid.name,
-                'user_address': order.uid.addr,
-                'product_name': item.pid.pname,
-                'product_rate': item.pid.rate,
-            })
-    context = {'all_order_details': all_order_details}
-    return render(request, 'admin_all_user_orders.html', context)
+    return render(request, 'admin_all_user_orders.html')
